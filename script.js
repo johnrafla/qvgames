13b60ead7ec6cfdb5941160cab07e8588c9d1318
Cherine Rafla <cherinerafla@Cherines-MacBook-Air.local>
2017-11-08 01:07:18 -0500

    added actual games

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ $(document).bind('keydown', function(e) {
       $('#maincharacter').html('<img src="jump.gif" height="90px" width="120px">');
       setTimeout (function() {
       $('#maincharacter').html('<img src="running.gif" height="80px">')
-        }, 900);
+        }, 990);
 
     // * Sliding Factor * //
   }
   else if (e.keyCode == 83 || e.keyCode == 40) {
-    $('#maincharacter').html('<img src="sliding.gif" height="90px" width="120px">');
+    $('#maincharacter').html('<img src="sliding.gif" height="100px" width="130px">');
     setTimeout (function() {
     $('#maincharacter').html('<img src="running.gif" height="80px">')
       }, 840);
@@ -19,10 +19,24 @@ $(document).bind('keydown', function(e) {
 });
 
 $(function() {
-  $('#startingframe').click(function() {    
+  $('#startingframe').click(function() {
   $('#startingframe').hide();
   $('#starting').hide();
   $('#starttext').hide();
   $('#running').show();
   });
+});
+
+$(function() {
+    $('#Flash_Games_Button').click(function() {
+    $('#Game_Container').hide();
+    $('#Flash_Games').show();
+    document.title = "Flash Games";
+    });
+
+    $('#My_Game_Button').click(function() {
+    $('#Game_Container').show('<iframe src="My Game.html" id="mainframe" scrolling="no"></iframe>');
+    $('#Flash_Games').hide();
+    document.title = "My Stickman Game";
+    });
 });
